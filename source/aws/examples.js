ffadf4b4414f1eb1e2a3610e7bc4f3dcc837993d
Jonathan <panagiotidisjonathan@gmail.com>
2016-04-16 17:02:34 +0300

    added some aws examples and functions

--- source/aws/examples.js
+++ source/aws/examples.js
@@ -1,40 +1,92 @@
 'use strict'
 
+import _ from 'underscore';
+import aws from 'aws-sdk';
 import config from '../config';
-import {init as initAWS, uploadFiles, createLambdaFunction} from './index.js';
+import {
+  getObjects,
+  uploadFile,
+  deleteBucketFiles
+} from './index.js';
+
+const test_bucket = "moufa";
 
 console.log('AWS EXAMPLES');
 console.log(config);
 
-initAWS(config.aws.users.jon);
+s3ExampleB()
+.then((res) => {
+  console.log(res);
+})
+.catch((err) => {
+  console.log(err);
+  console.log(err.stack);
+});
 
-function uploadFilesExample(){
-  uploadFiles(
+function init(){
+  aws.config.update(_.defaults(
     {
-      Bucket: "my-bucket",
-      ACL: 'public-read'
+      "region": "eu-west-1",
+      "signatureVersion": "v4"
     },
-    [
+    config.aws.users.jon
+  ));
+}
+
+async function createDummyBucketAndContents(){
+  try {
+    init();
+    const s3 = new aws.S3();
+    let buckets = await s3.listBuckets().promise();
+    if(_.filter(buckets.Buckets, (res) => {
+      return res.Name === test_bucket;
+    }).length > 0){
+      console.log('BUCKET ALREADY EXISTS');
+    }else{
+      console.log('CREATING BUCKET');
+      await s3.createBucket({
+        "Bucket": test_bucket
+      }).promise();
+    }
+
+    await uploadFile(
+      s3,
+      "/Users/jpanagiotidis/Documents/test_data/a.txt",
       {
-        "key": "doc-a.jpg",
-        "path": "/Users/jon/Documents/a.jpg"
-      },
-      {
-        "key": "doc-b.jpg",
-        "path": "/Users/jon/Documents/b.jpg"
-      },
-      {
-        "key": "doc-c.jpg",
-        "path": "/Users/jon/Documents/c.jpg"
+        "Bucket": test_bucket,
+        "Key": "test_b.txt",
+        "ACL": 'public-read'
       }
-    ]
-  )
-  .then((res) => {
-    console.log('UPLOAD COMPLETED', res);
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+    );
+
+    await uploadFile(
+      s3,
+      "/Users/jpanagiotidis/Documents/test_data/images/user.jpg",
+      {
+        "Bucket": test_bucket,
+        "Key": "test_c.jpg",
+        "ACL": 'public-read'
+      }
+    );
+    const list = await getObjects(s3, test_bucket, 3);
+    return "AWS S3 DUMMY BUCKET CREATED";
+  } catch (e) {
+    throw e;
+  }
+}
+
+async function deleteBucket(){
+  try {
+    init();
+    const s3 = new aws.S3();
+    await deleteBucketFiles(s3, test_bucket);
+    await s3.deleteBucket({
+      "Bucket": test_bucket
+    }).promise();
+    return "AWS S3 DUMMY BUCKET DELETED";
+  } catch (e) {
+    throw e;
+  }
 }
 
 // createLambdaFunction()
--- source/aws/index.js
+++ source/aws/index.js
@@ -1,35 +1,65 @@
 'use strict';
 
-import aws from 'aws-sdk';
 import _ from 'underscore';
 import {createReadStream} from 'fs';
 
-export function init(aws_credentials){
-  aws.config.update(aws_credentials);
-  aws.config.update({
-    "region": "eu-west-1",
-    "signatureVersion": 'v4'
-  })
-}
-
-export async function uploadFiles(options, file_paths){
+export async function deleteBucket(bucket_name){
   try {
-    const out = [];
     const s3 = new aws.S3();
-    for (var i = 0; i < file_paths.length; i++) {
-      console.log(file_paths[i]);
-      const body = createReadStream(file_paths[i].path);
-      const res = await s3.putObject(_.defaults({
-        "Key": file_paths[i].key,
-        "Body": body
-      }, options)).promise();
-      out.push(res);
-    }
-
-    return out;
+    console.log(bucket_name);
+    const res = await s3.deleteBucket({
+      "Bucket": bucket_name
+    }).promise();
   } catch (e) {
     throw e;
   }
+}
+
+export async function uploadFile(s3, file_path, options){
+  try {
+    options.Body = createReadStream(file_path);
+    return await s3.putObject(options).promise();
+  } catch (e) {
+    throw e;
+  }
+}
+
+export async function getObjects(s3, bucket_name, size){
+  let out = [];
+  const defaultParams = {
+    "Bucket": bucket_name
+  };
+
+  let currentObj;
+  do {
+    let params = {};
+    if(currentObj){
+      params = _.extend(defaultParams, {
+        "Marker": out[out.length - 1].Key
+      });
+    }else{
+      params = _.extend({}, defaultParams);
+    }
+    currentObj = await s3.listObjects(params).promise();
+    out = out.concat(currentObj.Contents);
+  } while (currentObj.IsTruncated && (!size || out.length < size));
+
+  return out.slice(0, size);
+}
+
+export async function deleteBucketFiles(s3, bucket_name){
+  const list = await getObjects(s3, bucket_name);
+  const params = {
+    "Bucket": bucket_name,
+    "Delete": {
+      "Objects": _.map(list, (obj) => {
+        return {
+          "Key": obj.Key
+        };
+      })
+    }
+  }
+  return await s3.deleteObjects(params).promise();
 }
 
 export async function createLambdaFunction(){
