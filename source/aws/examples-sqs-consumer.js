1c1f9a6963e7267678104096fa94ac787d7ea7a6
Jonathan <panagiotidisjonathan@gmail.com>
2016-04-22 19:51:17 +0300

    added simple sqs polling

---
+++ source/aws/examples-sqs-consumer.js
@@ -0,0 +1,56 @@
+'use strict';
+
+import _ from 'underscore';
+import aws from 'aws-sdk';
+import config from '../config';
+
+console.log('AWS SQS EXAMPLE CONSUMER');
+console.log(config);
+
+sqsConsumer()
+.then((res) => {
+  console.log('FIN', res);
+})
+.catch((err) => {
+  console.log(err);
+  console.log(err.stack);
+});
+
+function init(){
+  aws.config.update(_.defaults(
+    {
+      "region": "eu-west-1",
+      "signatureVersion": "v4"
+    },
+    config.aws.users.tester
+  ));
+}
+
+async function sqsConsumer(){
+  try {
+    init();
+    const sqs = new aws.SQS();
+    const res = await sqs.createQueue({
+      "QueueName": "TestQ"
+    }).promise();
+    console.log(res.QueueUrl);
+
+    do {
+      console.log('NEW TRY');
+      const mesRes = await sqs.receiveMessage({
+        "QueueUrl": res.QueueUrl,
+        "WaitTimeSeconds": 5
+      }).promise();
+      // console.log(mesRes);
+      if(mesRes.Messages && _.isArray(mesRes.Messages)){
+        for (let i = 0; i < mesRes.Messages.length; i++) {
+          const msg = mesRes.Messages[i];
+          console.log('CONSUMED: ID', msg.MessageId, 'BODY:', msg.Body);
+          // await
+        }
+      }
+    } while (true);
+  } catch (e) {
+    throw e;
+  }
+}
